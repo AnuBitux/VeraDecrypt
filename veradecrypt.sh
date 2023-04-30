4a71aa08327384c1045e4139183994e8f1db4833
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2023-04-30 16:45:27 +0200

    Create veradecrypt.sh

---
+++ veradecrypt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo ' '
+echo VeraDecrypt tryes to unlock disks, volumes, partitions and containsers encrypted with Veracrypt, using a given list of passwords
+echo ' '
+echo example: VeraDecrypt.py -v /home/anubitux/temp/volume -p /home/anubitux/temp/password_list 
+echo ' '
+echo not intended to use with  massive bruteforce
+echo suggested with custom password lists
+echo ' '
+
+$SHELL
