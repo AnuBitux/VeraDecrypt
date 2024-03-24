fe3b90fb41620e7cfd006295bdda2c8a26373ade
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 22:33:19 +0100

    Update veradecrypt.sh

--- veradecrypt.sh
+++ veradecrypt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo ' '
+figlet VeraDecrypt
+echo ' '
 echo VeraDecrypt tryes to unlock disks, volumes, partitions and containsers encrypted with Veracrypt, using a given list of passwords
 echo ' '
-echo example: VeraDecrypt.py -v /home/anubitux/temp/volume -p /home/anubitux/temp/password_list 
+echo example: veradecrypt.py -v /home/anubitux/temp/volume -p /home/anubitux/temp/password_list 
 echo ' '
 echo not intended to use with  massive bruteforce
 echo suggested with custom password lists
